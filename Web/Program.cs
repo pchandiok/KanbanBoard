802aaae33975bdf09f3cd48e8fb5687b18cc1624
Puneet Chandiok <pchandiok@foss.fun>
2023-07-17 12:06:17 +0530

    Functional Kanban Bozrd skeleton

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,6 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if(!app.Environment.IsDevelopment())
+{
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+
+app.MapRazorPages();
 
 app.Run();
